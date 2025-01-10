240bb3e4477de679160c61fa18a6326df04e628c
Askar.A <aalyiev7@gmail.com>
2025-01-10 15:57:51 +0500

    init

---
+++ src/test/java/kz/merchantservice/MerchantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.merchantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
